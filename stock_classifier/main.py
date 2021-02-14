d05d15f16c707c22626d52b4e0e1fbf80df4f2c4
fightnyy <fightnyy@naver.com>
2021-02-14 14:45:11 +0000

    kotokenizer

--- stock_classifier/main.py
+++ stock_classifier/main.py
@@ -5,48 +5,34 @@ import sys
 sys.path.append('..')
 from tokenization_kobert import KoBertTokenizer
 from transformers import BertModel
-
-
-
+from torch.utils.data import DataLoader
 import torch
 import torch.nn as nn
-import pytorch-lightning as pl
-import naver_data import SentimentalData
-
+import pytorch_lightning as pl
+from naver_data import SentimentalData
+import pdb
 
 class BertSentimental(pl.LightningModule):
     def __init__(self):
         super().__init__()
-        self.bert = bert
-        self.linear = nn.Linear(bert.config.hidden_size, 2)
+        self.bert = BertModel.from_pretrained('monologg/kobert')
+        self.linear = nn.Linear(self.bert.config.hidden_size, 2)
+        self.tokenizer = KoBertTokenizer.from_pretrained('monologg/kobert')
 
-    def prepare_data(self):
-        tokenizer = KoBertTokenizer.from_pretrained('monologg/kobert')
+    def forward(self, input_ids, attention_mask,token_type_ids):
 
-        def _tokenize(x):
-            x['input_ids'] = tokenizer.encode(
-                    x[],
-                    max_length = 128,
-                    pad_to_length = True
-                    )
-            return x
-
-
-    def forward(self, input_ids, attention_mask, token_type_ids):
-
-        h, _, attn = self.bert(input_ids = input_ids, attention_mask = attention_mask, token_type_ids = token_type_ids)
+        outputs = self.bert(input_ids = input_ids, attention_mask = attention_mask, token_type_ids = token_type_ids)
+        pdb.set_trace()
         h_cls = h[:, 0]
-        logits = self.W(h_cls)
+        logits = self.linear(h_cls)
+        pdb.set_trace()
         return logits, attn
 
 
     def training_step(self, batch, batch_idx):
-        input_ids, attention_mask, token_type_ids, label = batch
-
-        y_hat, attn = self(input_ids, attention_mask, token_type_ids)
-
+        label, input_ids, attention_mask, token_type_ids = batch
+        y_hat, attn = self.forward(input_ids, attention_mask, token_type_ids)
         loss = F.cross_entropy(y_hat, label)
-
         tensorboard_logs = {'train_loss': loss}
         return {'loss': loss, 'log': tensorboard_logs}
 
@@ -55,7 +41,14 @@ class BertSentimental(pl.LightningModule):
         return torch.optim.Adam([p for p in self.parameters() if p.requires_grad], lr=2e-05, eps=1e-08)
 
     def train_dataloader(self):
+        
+        train_data = SentimentalData('data/ratings_train.json')
+        return DataLoader(train_data, batch_size = 64)
 
+    def test_dataloader(self):
+
+        test_data = SentimentalData('data/ratings_test.json')
+        return DataLoader(test_data, batch_size = 64)
 
 
 
@@ -64,15 +57,5 @@ class BertSentimental(pl.LightningModule):
 
 if __name__ == '__main__':
     news_classifier = BertSentimental()
-    trainer = pl.Trainer(gpus=-1)
+    trainer = pl.Trainer(gpus=[1, 2], accelerator = 'ddp', precision = 16)
     trainer.fit(news_classifier)
-    token = tokenizer.tokenize("[CLS] 한국어 모델을 공유합니다.[SEP] 여러분 모두 반가워요.")
-    ids = kotokenizer.convert_tokens_to_ids(token)
-    ids = torch.tensor(ids)
-    ids = torch.unsqueeze(ids, 0)
-    print(ids.size())
-    model = BertModel.from_pretrained("monologg/kobert")
-    output = model(ids)
-    print(f"last hidden : {output[0].size()}")
-    print(f"pooler : {output[1].size()}")
-    print(model.embeddings.word_embeddings)
--- stock_classifier/naver_data.py
+++ stock_classifier/naver_data.py
@@ -1,12 +1,13 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 import torch
-import tqdm
+from tqdm import tqdm
+import sys
+sys.path.append('..')
+import json
 from tokenization_kobert import KoBertTokenizer
-
-
-class SetimentalData(torch.utils.data.Datset):
-
+from typing import List
+class SentimentalData(torch.utils.data.Dataset):
     def __init__(self, infile):
         """
         데이터셋 전처리 해주는 부분
@@ -14,21 +15,21 @@ class SetimentalData(torch.utils.data.Datset):
         self.tokenizer = KoBertTokenizer.from_pretrained('monologg/kobert')
         self.sentence = [] 
         self.label =  []
-
+        self.sentence_dict = {}
         line_cnt = 0
+        self.token2idx = self.tokenizer.token2idx
+        self.idx2token = {v: k for k, v in self.token2idx.items()}
         with open(infile, "r") as f:
             for line in f:
                 line_cnt += 1
-
-
-        with open(infile, "r") as f:
-            for i, line in enumerate(tqdm(f, total = line_cnt, desc = f"Loading {infile}", unit ="lines")):
                 data = json.loads(line)
                 self.label.append(data["label"])
-                self.sentence.append([self.tokenizer.encode(p) for p in data["doc"]])
+                self.sentence.append(data["doc"])
+       
+        self.sentence_dict=self.encode_batch(self.sentence, 128)     
 
     def __len__(self):
-        assert len(self.label) == len(self.senctence) 
+        assert len(self.label) == len(self.sentence) 
         return len(self.label)
         """
         데이터셋의 길이. 즉 ,총 샘플의 수를 적어주는 부분
@@ -37,10 +38,48 @@ class SetimentalData(torch.utils.data.Datset):
     def __getitem__(self, idx):
         return (
             torch.tensor(self.label[idx]),
-            torch.tensor(self.sentence[idx])
+            torch.tensor(self.sentence_dict['input_ids'][idx]),
+            torch.tensor(self.sentence_dict['attention_mask'][idx]),
+            torch.tensor(self.sentence_dict['token_type_ids'][idx])
         )
 
 
         """
         데이터셋에서 특정 1개의 샘플을 가져오는 함수
         """
+
+    def encode_batch(self, x: List[str], max_length):
+        max_len = 0
+        result_tokenization = []
+
+        for i in x:
+            tokens = self.tokenizer.encode(i, max_length=max_length, truncation=True)
+            result_tokenization.append(tokens)
+
+            if len(tokens) > max_len:
+                max_len = len(tokens)
+
+        padded_tokens = []
+        for tokens in result_tokenization:
+            padding = (torch.ones(max_len) * self.token2idx["[PAD]"]).long()
+            padding[: len(tokens)] = torch.tensor(tokens).long()
+            padded_tokens.append(padding.unsqueeze(0))
+
+        padded_tokens = torch.cat(padded_tokens, dim=0).long()
+        mask_tensor = torch.ones(padded_tokens.size()).long()
+
+        attention_mask = torch.where(
+            padded_tokens == self.token2idx["[PAD]"], padded_tokens, mask_tensor * -1
+        ).long()
+        attention_mask = torch.where(
+            attention_mask == -1, attention_mask, mask_tensor * 0
+        ).long()
+        attention_mask = torch.where(
+            attention_mask != -1, attention_mask, mask_tensor
+        ).long()
+
+        return {
+            "input_ids": padded_tokens.long(),
+            "attention_mask": attention_mask.long(),
+            "token_type_ids" : torch.tensor(([[0]*128] * len(result_tokenization))).long()  
+        }
